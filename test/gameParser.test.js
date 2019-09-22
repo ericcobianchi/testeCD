fa8070d93a3c9d4febedd8c5b5e4d0e240e2052b
Eric Cobianchi <ericcobianchi@hotmail.com>
2019-09-22 13:37:08 -0300

    test: fix function deathCause name

--- test/gameParser.test.js
+++ test/gameParser.test.js
@@ -1,33 +1,33 @@
-gameParser = require("../src/gameParser/gameParser");
+gameParser = require('../src/gameParser/gameParser');
 
-test("The killer must be batatinha22", () => {
+test('The killer must be batatinha22', () => {
   expect(
     gameParser.identifyKiller(
-      "13:08 Kill: 2 7 6: batatinha22 killed aaaaaa by MOD_ROCKET"
+      '13:08 Kill: 2 7 6: batatinha22 killed aaaaaa by MOD_ROCKET'
     )
-  ).toBe("batatinha22");
+  ).toBe('batatinha22');
 });
 
-test("the player who died must be arnold", () => {
+test('the player who died must be arnold', () => {
   expect(
     gameParser.identifyKilled(
-      "11:23 Kill: 2 5 6: Isgalamido killed arnold by MOD_ROCKET"
+      '11:23 Kill: 2 5 6: Isgalamido killed arnold by MOD_ROCKET'
     )
-  ).toBe("arnold");
+  ).toBe('arnold');
 });
 
-test("the player death cause", () => {
+test('the player death cause', () => {
   expect(
-    gameParser.identfyDeathCause(
-      " 11:41 Kill: 3 4 7: Oootsimo killed Dono da Bola by MOD_ROCKET_SPLASH"
+    gameParser.identifyDeathCause(
+      ' 11:41 Kill: 3 4 7: Oootsimo killed Dono da Bola by MOD_ROCKET_SPLASH'
     )
-  ).toBe("MOD_ROCKET_SPLASH");
+  ).toBe('MOD_ROCKET_SPLASH');
 });
 
-test("the player identification", () => {
+test('the player identification', () => {
   expect(
     gameParser.identifyPlayer(
       `21:53 ClientUserinfoChanged: 3 n\\Mocinha\\t\\0modelsargehmodelsargeg_redteam\\g_blueteam`
     )
-  ).toBe("Mocinha");
+  ).toBe('Mocinha');
 });
