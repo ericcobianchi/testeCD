b5e9f3d09821842cd1a2015fc790f96ce895b283
Eric Cobianchi <ericcobianchi@hotmail.com>
2019-09-22 13:21:14 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const logParser = require("./gameParser/gameParser.js");
+const logParser = require('./gameParser/gameParser.js');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("", (req, res) => {
+app.get('', (req, res) => {
   res.send(logParser.generateLogGame());
 });
 
