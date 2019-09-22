be138dea68b4d339ec018ba3da851c05248725ae
Eric Cobianchi <ericcobianchi@hotmail.com>
2019-09-21 21:17:23 -0300

    test: add identification test

--- test/gameParser.test.js
+++ test/gameParser.test.js
@@ -1,7 +1,17 @@
-function sum(a, b) {
-  return a + b;
-}
+gameParser = require("../src/gameParser/gameParser");
 
-test("the sum of 5+5 must be 10", () => {
-  expect(sum(5, 5)).toBe(10);
+test("The killer must be batatinha22", () => {
+  expect(
+    gameParser.identifyKiller(
+      "13:08 Kill: 2 7 6: batatinha22 killed aaaaaa by MOD_ROCKET"
+    )
+  ).toBe("batatinha22");
+});
+
+test("the player who died must be arnold", () => {
+  expect(
+    gameParser.identifykilled(
+      "11:23 Kill: 2 5 6: Isgalamido killed arnold by MOD_ROCKET"
+    )
+  ).toBe("arnold");
 });
